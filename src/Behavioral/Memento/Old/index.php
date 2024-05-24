91a3a198a9cf479310b37e3d2e30dc261a6f8b7e
salah-jr <mohammed.s.korna@gmail.com>
2024-05-24 23:39:27 +0200

    Adding: Memento DP

--- src/Behavioral/Memento/Old/index.php
+++ src/Behavioral/Memento/Old/index.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Complexity, the text editor is dependent on the state management and history
+ * Flexibility, I needed two stacks to deal with the content, so I will need others for any additional attributes
+ * Reusability, You will need to duplicate this code if you need it anywhere else.
+ */
+
 use src\Behavioral\Memento\Old\TextEditor;
 
 require_once __DIR__.'/../../../../vendor/autoload.php';
